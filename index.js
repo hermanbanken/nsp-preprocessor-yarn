b177740d7e4cd618e93ccd745b94a16ed5595bf0
Herman Banken <hermanbanken@gmail.com>
2017-10-27 11:42:52 +0200

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+const Fs = require('fs');
+const Path = require('path');
+const Lib = require('./lib.js');
+
+module.exports = {
+	check: function (args) {
+
+		let pkg;
+		try {
+			pkg = JSON.parse(Fs.readFileSync(Path.join(args.path, 'package.json')));
+		} catch (err) {
+			return Promise.reject(new Error(`Unable to load package.json for project: ${Path.basename(args.path)}`));
+		}
+
+		let lock;
+		try {
+			const lockContents = Fs.readFileSync(Path.join(args.path, 'yarn.lock'));
+			const lock = Lib.parse(lockContents, pkg);
+		} catch (err) {
+			return Promise.reject(new Error(`Unable to load yarn.lock for project: ${Path.basename(args.path)}`));
+		}
+
+		return Object.assign(args, { pkg: pkg, shrinkwrap: lock });
+	}
+};
---
+++ lib.js
@@ -0,0 +1,45 @@
+const Lockfile = require('@yarnpkg/lockfile');
+
+function yarnParse(contents, packageJson) {
+	var result = Lockfile.parse(contents);
+	var dependencies = Object.keys(packageJson).reduce(function (acc, key) {
+		return typeof packageJson[key] === 'object' && key !== 'engines' ?
+			Object.assign(acc, packageJson[key]) :
+			acc;
+	}, {});
+	return {
+		name: packageJson.name,
+		version: packageJson.version,
+		dependencies: buildTree(dependencies, result.object, [packageJson.name])
+	};
+}
+
+function buildTree(dependencies, yarn, path) {
+	return Object.keys(dependencies).reduce(function (acc, packageName) {
+
+		var version = dependencies[packageName];
+		var packageNV = packageName + '@' + version;
+		var info = yarn[packageNV];
+
+		if (!info) {
+			throw new Error('yarn.lock is outdated: it does not contain the package ' + packageNV);
+		}
+
+		if (path.indexOf(packageNV) >= 0) {
+			// Prevent looping over circular dependencies (for example babel-register@^6.26.0 > babel-core@^6.26.0 > babel-register@^6.26.0)
+			return acc;
+		}
+		var currentPath = path.slice(0);
+		currentPath.push(packageNV);
+
+		acc[packageName] = {
+			'version': info.version,
+			'dependencies': typeof info.dependencies === 'object' ?
+				buildTree(info.dependencies, yarn, currentPath) :
+				undefined
+		};
+		return acc;
+	}, {});
+}
+
+module.exports = { parse: parse, buildTree: buildTree };
