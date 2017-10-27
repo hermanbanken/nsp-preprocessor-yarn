dadb6fa6f40a784960a31eb34abbd4d0d5e9eefc
Herman Banken <hermanbanken@gmail.com>
2017-10-27 13:59:39 +0200

    integration test

---
+++ test/integration.js
@@ -0,0 +1,36 @@
+const exec = require("child_process").exec;
+const join = require("path").join;
+let errors = [];
+let oks = [];
+
+function contains(stderr, stdout, text) {
+	return stderr.indexOf(text) >= 0 || stdout.indexOf(text) >= 0;
+}
+
+const tests = [
+	exec("nsp --preprocessor yarn check", { cwd: join(process.cwd(), "test/data/sample") }, (err, stdout, stderr) => {
+		if (!contains(stderr, stdout, "3 vulnerabilities found")) {
+			errors.push("Should report vulnerabilities from yarn.lock.");
+		} else { oks.push("sample"); }
+		finish();
+	}),
+	exec("nsp --preprocessor yarn check", { cwd: join(process.cwd(), "test/data/circular") }, (err, stdout, stderr) => {
+		if (!contains(stderr, stdout, "3 vulnerabilities found")) {
+			errors.push("Should work, even with circular dependencies.");
+		} else { oks.push("circular"); }
+		finish();
+	}),
+	exec("nsp --preprocessor yarn check", { cwd: join(process.cwd(), "test/data/outdated") }, (err, stdout, stderr) => {
+		if (!contains(stderr, stdout, "yarn.lock is outdated")) {
+			errors.push("Should report yarn.lock files that are not in sync with package.json." + err + stderr + stdout);
+		} else { oks.push("outdated"); }
+		finish();
+	})
+];
+
+function finish() {
+	if (oks.length + errors.length < tests.length) { return; }
+	oks.forEach(test => console.info("✅  " + test + " ran successfully"));
+	errors.forEach(error => console.error("🆘  " + error));
+	if (errors.length > 0) { process.exit(1); }
+}
